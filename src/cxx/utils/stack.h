2bb8380a1e4344f1d723caef7693e30a7e0448fe
Treyvon Whitaker <ixione.dev@gmail.com>
2026-01-04 10:07:46 -0600

    feat(dsa): expose c abi wrapper for stack

---
+++ src/cxx/utils/stack.h
@@ -0,0 +1,49 @@
+#ifndef STACK_H
+#define STACK_H
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#define InitialStackCapacity 10
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct Stack_t Stack_t;
+
+typedef enum { StackI32, StackPtr } StackType;
+
+typedef struct {
+    StackType type;
+    union {
+        int32_t i32;
+        void *ptr;
+    } as;
+} StackValue;
+
+#define NULL_VALUE ((StackValue){StackPtr, {.ptr = NULL}})
+
+#define IsI32(value) ((value).type == StackI32)
+#define IsPtr(value) ((value).type == StackPtr)
+
+#define AsI32(value) ((value).as.i32)
+#define AsPtr(value) ((value).as.ptr)
+
+Stack_t *NewStack(StackType type);
+Stack_t *NewStackWithCapacity(StackType type, size_t c);
+void DeleteStack(Stack_t *s);
+
+int StackPush(Stack_t *s, StackValue value);
+StackValue StackPop(Stack_t *s);
+StackValue *StackPeek(Stack_t *s);
+
+size_t StackSize(const Stack_t *s);
+bool StackIsEmpty(const Stack_t *s);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // STACK_H
---
+++ src/cxx/utils/stack_test.cpp
@@ -0,0 +1,45 @@
+#include <gtest/gtest.h>
+
+#include "stack.h"
+
+TEST(StackTest, TestStackPush) {
+    Stack_t *s = NewStack(StackI32);
+
+    StackPush(s, StackValue{StackI32, {.i32 = 1}});
+    StackPush(s, StackValue{StackI32, {.i32 = 2}});
+
+    ASSERT_EQ(2, StackSize(s));
+    DeleteStack(s);
+
+    s = NewStack(StackI32);
+
+    StackPush(s, StackValue{StackI32, {.i32 = 3}});
+    StackPush(s, StackValue{StackI32, {.i32 = 4}});
+    StackPush(s, StackValue{StackI32, {.i32 = 5}});
+
+    ASSERT_EQ(3, StackSize(s));
+    DeleteStack(s);
+
+    s = NewStack(StackI32);
+
+    ASSERT_EQ(0, StackSize(s));
+    DeleteStack(s);
+
+    s = NewStack(StackI32);
+
+    StackPush(s, StackValue{StackI32, {.i32 = 6}});
+    StackPush(s, StackValue{StackI32, {.i32 = 7}});
+    StackPush(s, StackValue{StackI32, {.i32 = 8}});
+    StackPush(s, StackValue{StackI32, {.i32 = 9}});
+
+    ASSERT_EQ(4, StackSize(s));
+    DeleteStack(s);
+
+    s = NewStack(StackI32);
+
+    StackPush(s, StackValue{StackI32, {.i32 = 10}});
+    StackPush(s, StackValue{StackI32, {.i32 = 11}});
+
+    ASSERT_EQ(2, StackSize(s));
+    DeleteStack(s);
+}
